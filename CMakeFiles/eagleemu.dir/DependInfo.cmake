0e306a816ef9c76ee457f3c1cd0a8175a9e91bc6
clement <clementchpoon@gmail.com>
2026-05-03 00:45:27 +0100

    add missing buildsystme tiles

---
+++ CMakeFiles/eagleemu.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/clement/Desktop/eagleemu/eagleemu/8255.c" "CMakeFiles/eagleemu.dir/8255.c.o" "gcc" "CMakeFiles/eagleemu.dir/8255.c.o.d"
+  "/home/clement/Desktop/eagleemu/eagleemu/cpu.c" "CMakeFiles/eagleemu.dir/cpu.c.o" "gcc" "CMakeFiles/eagleemu.dir/cpu.c.o.d"
+  "/home/clement/Desktop/eagleemu/eagleemu/gfx.c" "CMakeFiles/eagleemu.dir/gfx.c.o" "gcc" "CMakeFiles/eagleemu.dir/gfx.c.o.d"
+  "/home/clement/Desktop/eagleemu/eagleemu/keyb.c" "CMakeFiles/eagleemu.dir/keyb.c.o" "gcc" "CMakeFiles/eagleemu.dir/keyb.c.o.d"
+  "/home/clement/Desktop/eagleemu/eagleemu/main.c" "CMakeFiles/eagleemu.dir/main.c.o" "gcc" "CMakeFiles/eagleemu.dir/main.c.o.d"
+  "/home/clement/Desktop/eagleemu/eagleemu/saa1099_ext.c" "CMakeFiles/eagleemu.dir/saa1099_ext.c.o" "gcc" "CMakeFiles/eagleemu.dir/saa1099_ext.c.o.d"
+  "/home/clement/Desktop/eagleemu/eagleemu/sd.c" "CMakeFiles/eagleemu.dir/sd.c.o" "gcc" "CMakeFiles/eagleemu.dir/sd.c.o.d"
+  "/home/clement/Desktop/eagleemu/eagleemu/serial.c" "CMakeFiles/eagleemu.dir/serial.c.o" "gcc" "CMakeFiles/eagleemu.dir/serial.c.o.d"
+  "" "eagleemu" "gcc" "CMakeFiles/eagleemu.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/eagleemu.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for eagleemu.
+# This may be replaced when dependencies are built.
